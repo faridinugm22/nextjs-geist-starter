c93b45b72024c7bf26b212ee514dc2fad37e77bf
faridinugm22 <160242027+faridinugm22@users.noreply.github.com>
2025-07-08 11:16:36 +0700

    Add .next/static/chunks/src_app_management_sms_page_tsx_5e7cb368._.js 1751948196237

---
+++ .next/static/chunks/src_app_management_sms_page_tsx_5e7cb368._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_61fedad4._.js",
+  "static/chunks/node_modules_18b2e60b._.js"
+],
+    source: "dynamic"
+});
